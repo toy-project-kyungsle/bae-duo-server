67a70b7cbe316b0ebe467634fec0a25b7e40c536
keinn51 <soogood97@gmail.com>
2023-03-15 00:12:12 +0900

    feat : cors 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import connection from './db';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  await app.listen(3050);
 }
 bootstrap();
