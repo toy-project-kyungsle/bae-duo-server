01f4f15d409620864c88bc8f10b549a4d0348a03
J-vInyL <wnstjq7894@gmail.com>
2023-03-29 00:16:18 +0900

    auth controller 작업, jwt 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Res, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export default class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleAuth() {
+    // initiates the Google OAuth2 login flow
+  }
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleLoginCallback(@Req() req, @Res() res) {
+    // handles the Google OAuth2 callback
+    const jwt = req.user.jwt;
+    if (jwt) res.redirect(`http://localhost:3000?jwt=${jwt}`);
+    else res.redirect(`http://localhost:3000?error=login_failed`);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import AuthController from './auth.controller';
 import { AuthService } from './auth/auth.service';
 import { GoogleStrategy } from './google.strategy';
 
