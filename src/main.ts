417fa922d17a40d6489f19ffafb384159d987724
J-vInyL <wnstjq7894@gmail.com>
2023-05-08 21:13:01 +0900

    SSL 인증 경로 및 fs 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync(process.env.SSLKEY),
+  cert: fs.readFileSync(process.env.SSLCERT),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
     origin: '*',
     credentials: true,
