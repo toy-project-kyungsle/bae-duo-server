a09f82558e8ca8d8c5dba0d3899ff64b37346994
keinn51 <soogood97@gmail.com>
2023-05-08 21:23:26 +0900

    feat: httpsOptions 예외처리, 로컬에서도 돌아가도록

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 
-const httpsOptions = {
-  key: fs.readFileSync(process.env.SSLKEY),
-  cert: fs.readFileSync(process.env.SSLCERT),
-};
+let httpsOptions = null;
+
+try {
+  httpsOptions = {
+    key: fs.readFileSync(process.env.SSLKEY || ''),
+    cert: fs.readFileSync(process.env.SSLCERT || ''),
+  };
+} catch {
+  console.log("can't find key and cert");
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { httpsOptions });
