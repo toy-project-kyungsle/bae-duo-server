a0e7e56b7702bb4521c8d3663753335b9cd3a3df
keinn51 <soogood97@gmail.com>
2023-05-16 22:01:24 +0900

    feat: attendant menu info 를 삭제하면 funding 업데이트

--- src/attendantMenuInfo/attendantMenuInfo.controller.ts
+++ src/attendantMenuInfo/attendantMenuInfo.controller.ts
@@ -2,10 +2,16 @@ import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AttendantMenuInfoService } from './attendantMenuInfo.service';
 import { CreateAttendantMenuInfoDto } from './attendantMenuInfo.dto';
 import { attendantMenuInfoType } from './attendantMenuInfo.type';
+import { AttendantService } from 'src/attendant/attendant.service';
+import { FundingService } from 'src/funding/funding.service';
 
 @Controller('attendantMenuInfo')
 export class AttendantMenuInfoController {
-  constructor(private attendantMenuInfoService: AttendantMenuInfoService) {}
+  constructor(
+    private attendantService: AttendantService,
+    private attendantMenuInfoService: AttendantMenuInfoService,
+    private fundingService: FundingService,
+  ) {}
 
   @Post()
   async saveAttendantMenuInfo(@Body() sentData: CreateAttendantMenuInfoDto) {
@@ -27,7 +33,23 @@ export class AttendantMenuInfoController {
   }
 
   @Delete('/:id')
-  deleteAttendantMenuInfoById(@Param('id') id: number): Promise<number> {
+  async deleteAttendantMenuInfoById(@Param('id') id: number): Promise<number> {
+    const targetMunuInfo =
+      await this.attendantMenuInfoService.findAttendantMenuInfosById(id);
+    const targetAttendant = await this.attendantService.findAttendantById(
+      targetMunuInfo.attendantId,
+    );
+    const targetFunding = await this.fundingService.findFundingById(
+      targetAttendant.fundingId,
+    );
+
+    // menu의 가격에 맞게 funding의 curPrice를 낮추어줌
+    targetFunding['curPrice'] -= targetMunuInfo.menuPrice;
+
+    const putTargetFunding = await this.fundingService.updateFunding(
+      targetFunding,
+    );
+
     return this.attendantMenuInfoService.deleteAttendantMenuInfoById(id);
   }
 }
--- src/attendantMenuInfo/attendantMenuInfo.module.ts
+++ src/attendantMenuInfo/attendantMenuInfo.module.ts
@@ -4,9 +4,24 @@ import { AttendantMenuInfoController } from './attendantMenuInfo.controller';
 import { AttendantMenuInfoService } from './attendantMenuInfo.service';
 import { AttendantMenuInfo as AttendantMenuInfoEntity } from './attendantMenuInfo.entity';
 
+import { Funding as FundingEntity } from 'src/funding/funding.entity';
+import { FundingController } from 'src/funding/funding.controller';
+import { FundingService } from 'src/funding/funding.service';
+import { Attendant as AttendantEntity } from 'src/attendant/attendant.entity';
+import { AttendantController } from 'src/attendant/attendant.controller';
+import { AttendantService } from 'src/attendant/attendant.service';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([AttendantMenuInfoEntity])],
-  controllers: [AttendantMenuInfoController],
-  providers: [AttendantMenuInfoService],
+  imports: [
+    TypeOrmModule.forFeature([AttendantEntity]),
+    TypeOrmModule.forFeature([AttendantMenuInfoEntity]),
+    TypeOrmModule.forFeature([FundingEntity]),
+  ],
+  controllers: [
+    AttendantController,
+    AttendantMenuInfoController,
+    FundingController,
+  ],
+  providers: [AttendantService, AttendantMenuInfoService, FundingService],
 })
 export class AttendantMenuInfoModule {}
