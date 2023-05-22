c7765a6fad6e654476c8a5d869a01381eaa56cdc
sseung95 <seung3657@gmail.com>
2023-05-22 22:02:09 +0900

    brands 이미지 업로드 기능 추가

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,52 +1,28 @@
 import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Brands } from './brands.entity';
-import { Uploads } from './brands.uploads.entity';
 import { Repository } from 'typeorm';
 import { CreateBrandsDto } from './dto/create-brand.dto';
 import { UpdateBrandsDto } from './dto/update-brand.dto';
-//import * as AWS from 'aws-sdk';
-
-//const s3 = new AWS.S3({ useAccelerateEndpoint: true });
+import { UploadsService } from 'src/uploads/uploads.service';
 
 @Injectable()
 export class BrandsService {
   constructor(
     @InjectRepository(Brands)
     private brandsRepository: Repository<Brands>,
+    private uploadsService: UploadsService,
   ) {}
 
-  async saveBrand(sentData: CreateBrandsDto, file: any): Promise<Brands> {
-    if (file) {
-      // AWS.config.update({
-      //   region: 'ap-northeast-2',
-      //   credentials: {
-      //     accessKeyId: process.env.IAMAccess,
-      //     secretAccessKey: process.env.IAMSecret,
-      //   },
-      // });
-      // try {
-      //   const params = {
-      //     Bucket: process.env.BUCKET_NAME,
-      //     Key: `${Date.now() + file.originalname}`,
-      //     Body: file.buffer,
-      //     ContentType: 'image/jpeg',
-      //     ACL: 'public-read',
-      //   };
-      //   await new AWS.S3().putObject(params).promise();
-      //   const s3Url = await s3.getSignedUrlPromise('putObject', params);
-      //   file.url = s3Url;
-      //   file.createdId = `${Date.now() + file.originalname}`;
-      //   this.saveUploads(file);
-      //   return  file.originalname
-      // } catch (error) {
-      //   console.error('ERROR : ', error);
-      // }
-    }
+  async saveBrand(
+    sentData: CreateBrandsDto,
+    file: Express.Multer.File,
+  ): Promise<Brands> {
+    const fileUrl = file ? await this.uploadsService.uploadFile(file) : null;
 
     const instance = await this.brandsRepository.save({
       ...sentData,
-      brandImage: '',
+      brandImage: fileUrl,
     });
 
     if (!instance) {
@@ -79,12 +55,8 @@ export class BrandsService {
   async updateBrand(
     id: number,
     sentData: UpdateBrandsDto,
-    file?: File,
+    file?: Express.Multer.File,
   ): Promise<Brands> {
-    if (file) {
-      console.log('File');
-    }
-
     const brand = await this.brandsRepository.findOne({
       where: { id },
     });
@@ -100,6 +72,11 @@ export class BrandsService {
       if (['id', 'createdUserId', 'createdAt', 'file'].includes(key)) return;
       brand[key] = sentData[key];
     });
+
+    brand.brandImage = file
+      ? await this.uploadsService.uploadFile(file)
+      : brand.brandImage;
+
     await this.brandsRepository.update(id, brand);
     return brand;
   }
@@ -111,19 +88,4 @@ export class BrandsService {
     }
     return HttpStatus.ACCEPTED;
   }
-
-  // async saveUploads(sentData: File): Promise<Uploads> {
-  //   console.log('SENT', sentData);
-  //   const instance = await this.uploadsRepository.save(sentData);
-  //   if (!instance) {
-  //     throw new NotFoundException(`업로드 리스트를 생성할 수 없습니다.`);
-  //   }
-  //   return instance;
-  // }
-
-  // async findUploadsById(name: string): Promise<Uploads> {
-  //   const upload = await this.uploadsRepository.findOne({ where: { name } });
-  //   if (!upload) throw new NotFoundException(`업로드 파일을 찾을 수 없습니다.`);
-  //   return upload;
-  // }
 }
--- src/uploads/uploads.service.ts
+++ src/uploads/uploads.service.ts
@@ -1,8 +1,9 @@
-import { HttpStatus, NotFoundException, Injectable } from '@nestjs/common';
+import { NotFoundException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Uploads } from './uploads.entity';
 import { Repository } from 'typeorm';
 import { CreateUploadsDto } from '../brands/dto/create-uploads.dto';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class UploadsService {
@@ -24,5 +25,39 @@ export class UploadsService {
     const upload = await this.uploadsRepository.findOne({ where: { name } });
     if (!upload) throw new NotFoundException(`업로드 파일을 찾을 수 없습니다.`);
     return upload;
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    AWS.config.update({
+      region: 'ap-northeast-2',
+      accessKeyId: process.env.IAMAccess,
+      secretAccessKey: process.env.IAMSecret,
+    });
+    try {
+      const key = `${Date.now() + file.originalname}`;
+      const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: key,
+        Body: file.buffer,
+        ContentType: 'image/jpeg',
+        ACL: 'public-read',
+      };
+      await new AWS.S3().putObject(params).promise();
+
+      const files = {
+        createdId: key,
+        name: file.originalname,
+        extension: file.mimetype,
+        size: file.size,
+        url: `https://baeduo.s3.ap-northeast-2.amazonaws.com/${key}`,
+      };
+
+      console.log('files', file);
+      await this.saveUploads(files);
+      return files.url;
+    } catch (error) {
+      console.error('ERROR : ', error);
+    }
+    return '';
   }
 }
