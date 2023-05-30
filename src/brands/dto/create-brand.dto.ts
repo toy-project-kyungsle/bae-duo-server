5716bdbb041a1d860520f30130f2c3b4eaa1019b
sseung95 <seung3657@gmail.com>
2023-05-31 00:47:13 +0900

    update dto 중 brandImage 타입 제거

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,15 +1,8 @@
-// import { IsNumber, IsString } from 'class-validator';
-
 export class CreateBrandsDto {
-  // @IsNumber()
   createdUserId: number;
-  // @IsString()
   name: string;
-  // @IsNumber()
   orderType: number;
   defaultDeadLine: Date | null;
-  // @IsNumber()
   defaultMinPrice: number | null;
-  // @IsNumber()
   defaultMinMember: number | null;
 }
--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -2,7 +2,7 @@ export class UpdateBrandsDto {
   createdUserId: number;
   name: string; // 브랜드명
   orderType: number; // 배달 or 포장
-  brandImage: string;
+  // brandImage: string;
   defaultDeadLine: Date | null;
   defaultMinPrice: number | null;
   defaultMinMember: number | null;
